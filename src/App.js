cf84364e8d010871627de468ba001bf0381fc01b
Bhawna Keswani <37388011+simmi9@users.noreply.github.com>
2023-10-10 16:51:11 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 const App = () => {
+  const categories = [
+    { id: "1", title: "Hats" },
+    { id: "2", title: "Mens" },
+    { id: "3", title: "Sneakers" },
+    { id: "4", title: "Womens" },
+  ];
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+    <div className="categories-container">
+      {categories.map(({ title }) => {
+        return (
+          <div className="category-container">
+            {/* <img /> */}
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
